800f702e6878caeaf9ed85530849bf4122edbc6d
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-03-30 17:43:55 +0400

    Updated CV

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,14 +11,14 @@ const Contact = () => {
   // Function will execute on click of button
   const onButtonClick = () => {
     // using Java Script method to get PDF file
-    fetch("mhmd_CV.pdf").then((response) => {
+    fetch("MhmdDorgham.pdf").then((response) => {
       response.blob().then((blob) => {
         // Creating new object of PDF file
         const fileURL = window.URL.createObjectURL(blob);
         // Setting various property values
         let alink = document.createElement("a");
         alink.href = fileURL;
-        alink.download = "mhmd_CV.pdf";
+        alink.download = "MhmdDorgham.pdf";
         alink.click();
       });
     });
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,14 +9,14 @@ const Navbar = () => {
   // Function will execute on click of button
   const onButtonClick = () => {
     // using Java Script method to get PDF file
-    fetch("mhmd_CV.pdf").then((response) => {
+    fetch("MhmdDorgham.pdf").then((response) => {
       response.blob().then((blob) => {
         // Creating new object of PDF file
         const fileURL = window.URL.createObjectURL(blob);
         // Setting various property values
         let alink = document.createElement("a");
         alink.href = fileURL;
-        alink.download = "mhmd_CV.pdf";
+        alink.download = "MhmdDorgham.pdf";
         alink.click();
       });
     });
