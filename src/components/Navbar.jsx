ee734f3e0929cf6bf5a5840d448a3229d97f1959
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-03-30 14:59:41 +0400

    Updated Content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
             </div>
             <div className="border-b border-gray-300 my-4 ">
               <p className="w-[85%] md:w-[90%] py-4">
-                Let's build something legendary together
+                Let us build something legendary together
               </p>
             </div>
           </div>
@@ -136,7 +136,7 @@ const Navbar = () => {
               </ul>
               <div className="pt-20">
                 <p className="uppercase tracking-widest text-[#5651e5]">
-                  Let's Connect
+                  Let us Connect
                 </p>
                 <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
                   <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
